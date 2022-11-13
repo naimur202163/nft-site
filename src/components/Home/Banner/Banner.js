9bbd8f0ac5731d26264ea953427613264dce349c
naimur <naimur202163@gmail.com>
2022-11-13 08:26:40 +0600

    working on gird systeam

--- src/components/Home/Banner/Banner.js
+++ src/components/Home/Banner/Banner.js
@@ -1,9 +1,16 @@
-import React from 'react'
+import React from "react";
 
 export default function Banner() {
   return (
     <div>
-        
+      <div className="grid grid-cols-3 gap-4">
+        <div>1</div>
+        <div>2</div>
+        <div>3</div>
+        <div>4</div>
+        <div>5</div>
+        <div>6</div>
+      </div>
     </div>
-  )
+  );
 }
