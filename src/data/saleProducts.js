14a7378daec6208a3b679aab626ed4ca8010042e
naimur <naimur202163@gmail.com>
2022-11-16 07:11:42 +0600

    creating fake data

---
+++ src/data/saleProducts.js
@@ -0,0 +1,15 @@
+[
+    {
+        "img":"https://i.ibb.co/dLpnPgz/p-26-md.png",
+        "title":"Adidas Shoes Black",
+        "description":"Men Black top sleeveless gown",
+        "price":"45.00"
+    },
+    {
+        "img":"https://i.ibb.co/TvrzBPq/p-16-md.webp",
+        "title":"Armani Wide-Leg Trousers",
+        "description":"Monochrome elegance. Made with a relaxed wide-leg, these trousers are made from a sustainable soft organic cotton with a mechanical stretch making the garment easily recycled.",
+        "price":"45.00"
+    },
+
+]
