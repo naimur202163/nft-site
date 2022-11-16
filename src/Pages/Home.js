31f990d4f7f3a577af57aa1837986699ff9c1946
naimur <naimur202163@gmail.com>
2022-11-16 20:15:15 +0600

    done

--- src/Pages/Home.js
+++ src/Pages/Home.js
@@ -1,5 +1,6 @@
 import React from "react";
 import Banner from "../components/Home/Banner/Banner";
+import CategorySlider from "../components/Home/CategorySlider/CategorySlider";
 import FlashSale from "../components/Home/FlashSale/FlashSale";
 import Slider from "../components/Home/Slider/Slider";
 import Footer from "../Shared/Footer/Footer";
@@ -12,6 +13,7 @@ export default function Home() {
       <Banner />
       <FlashSale />
       <Slider />
+      <CategorySlider/>
       <Footer />
     </div>
   );
---
+++ src/components/Home/CategorySlider/CategorySlider.js
@@ -0,0 +1,11 @@
+import React from 'react'
+
+export default function CategorySlider() {
+  return (
+    <div className='mx-auto sm:max-w-xl md:max-w-full lg:max-w-screen-xl'>
+
+
+      
+    </div>
+  )
+}
