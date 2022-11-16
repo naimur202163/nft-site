504b9052b6bc1eb9e53bec349f7d27c81401dd49
naimur <naimur202163@gmail.com>
2022-11-16 07:34:20 +0600

    data added

--- src/data/saleProducts.js
+++ src/data/saleProducts.js
@@ -1,15 +1,73 @@
 [
-    {
-        "img":"https://i.ibb.co/dLpnPgz/p-26-md.png",
-        "title":"Adidas Shoes Black",
-        "description":"Men Black top sleeveless gown",
-        "price":"45.00"
-    },
-    {
-        "img":"https://i.ibb.co/TvrzBPq/p-16-md.webp",
-        "title":"Armani Wide-Leg Trousers",
-        "description":"Monochrome elegance. Made with a relaxed wide-leg, these trousers are made from a sustainable soft organic cotton with a mechanical stretch making the garment easily recycled.",
-        "price":"45.00"
-    },
+  {
+    "img": "https://i.ibb.co/dLpnPgz/p-26-md.png",
+    "title": "Adidas Shoes Black",
+    "description": "Men Black top sleeveless gown",
+    "price": "45.00",
+   
+  
+  },
+  {
+    "img": "https://i.ibb.co/TvrzBPq/p-16-md.webp",
+    "title": "Armani Wide-Leg Trousers",
+    "description":
+      "Monochrome elegance. Made with a relaxed wide-leg, these trousers are made from a sustainable soft organic cotton with a mechanical stretch making the garment easily recycled.",
+    "price": "120.00",
+  },
+  {
+    "img": "https://i.ibb.co/XXcX4PL/p-3.webp",
+    "title": "Zara Shoes Green",
+    "description":
+      "Footwear refers to garments worn on the feet, which originally serves to purpose of protection against adversities of the environment, usually regarding ground textures and temperature.",
 
-]
+    "price": "50.00",
+  },
+  {
+    "img": "https://i.ibb.co/gzX2xTf/p-25-md.png",
+    "title": "Wayfarer Sunglasses",
+    "description":
+      "Our optical engineers developed these sunglasses for hiking. Ideal for occasional use in the mountains.",
+
+    "price": "30.00",
+  },
+  {
+    "img": "https://i.ibb.co/xCf01Sq/p-14-md.webp",
+    "title": "Hermes Carlton London",
+    "description":
+      "Off-White self-striped knitted midi A-line dress, has a scoop neck, sleeveless, straight hem",
+
+    "price": "15.00",
+  },
+  {
+    "img": "https://i.ibb.co/429TH2t/p-27-md.png",
+    "title": "Polarised Wayfarer Sunglasses",
+    "description":
+      "This item is only exchangeable for the same or a different size, if available, and cannot be returned",
+
+    "price": "20.00",
+  },
+  {
+    "img": "https://i.ibb.co/HX4q9Jx/p-8-md.png",
+    "title": "Gucci Carlton UK",
+    "description":
+      "Knitted midi A-line dress, has a scoop neck, sleeveless, straight hem",
+
+    "price": "14.99",
+  },
+  {
+    "img": "https://i.ibb.co/60hWPj8/p-24-md.webp",
+    "title": "NIKE Shoes",
+    "description":
+      "NIKE 2020 Black White is a clean and monochromatic colourway of the label’s latest high-technology silhouette. The model first launched late last year and is currently Jordan Brand’s flagship performance pair.",
+
+    "price": "50.00",
+  },
+  {
+    "img": "https://i.ibb.co/vYrLRpx/p-12-md.png",
+    "title": "Wayfarer Sunglasses",
+    "description":
+      "Our optical engineers developed these sunglasses for hiking. Ideal for occasional use in the mountains.",
+
+    "price": "20.00",
+  },
+];
