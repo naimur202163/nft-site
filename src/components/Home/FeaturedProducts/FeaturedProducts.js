7d1d9694c4879f41936385ff6823ddeb34c5bf72
naimur <naimur202163@gmail.com>
2022-11-17 12:43:22 +0600

    feature section done

--- src/components/Home/FeaturedProducts/FeaturedProducts.js
+++ src/components/Home/FeaturedProducts/FeaturedProducts.js
@@ -55,8 +55,8 @@ export default function FeaturedProducts() {
   return (
     <div className="mx-auto sm:max-w-xl md:max-w-full lg:max-w-screen-xl my-14">
       <h2 className="font-opens font-bold text-3xl">Featured Products</h2>
-      <div className="grid grid-cols-2 gap-4">
-        <div className="bg-light-gray ">
+      <div className="grid sm:grid-cols-1  mds:grid-cols-1  lg:grid-cols-2 gap-4">
+        <div className="bg-light-gray p-2">
           {data.slice(0, 1).map((items) => (
             <Link to={`/featured/${items.id}`}>
               <img
@@ -64,52 +64,128 @@ export default function FeaturedProducts() {
                 src={items.img}
                 alt={items.title}
               />
+              <div className="flex justify-between mt-20">
+                <h2 className="font-opens font-semibold text-xl">
+                  {items.title}
+                </h2>
+                <h2 className="font-opens font-semibold text-md text-gray-600 text-sm md:text-base lg:text-sm xl:text-base 3xl:text-lg">
+                  $20.00
+                </h2>
+              </div>
+              <div className="flex justify-between my-2 align-middle gap-1">
+                <p className="font-opens text-gray-600 text-sm mt-2">
+                  {items.description.slice(0, 30)}...
+                </p>
+                <h2 className="font-opens font-semibold text-md   text-2xl">
+                  ${items.price}
+                </h2>
+              </div>
             </Link>
           ))}
         </div>
         <div className="grid grid-cols-2 gap-4">
           <div className=" bg-light-gray">
             {data.slice(1, 2).map((items) => (
-              <div>
+              <Link className="p-2" to={`/featured/${items.id}`}>
                 <img
                   className="scale-100 hover:scale-110 ease-in duration-500"
                   src={items.img}
                   alt={items.title}
                 />
-              </div>
+                <div className="flex justify-between">
+                  <h2 className="font-opens font-semibold text-xl">
+                    {items.title}
+                  </h2>
+                  <h2 className="font-opens font-semibold text-md text-gray-600 text-sm md:text-base lg:text-sm xl:text-base 3xl:text-lg">
+                    $20.00
+                  </h2>
+                </div>
+                <div className="flex justify-between my-2 align-middle gap-1">
+                  <p className="font-opens text-gray-600 text-sm mt-2">
+                    {items.description.slice(0, 30)}...
+                  </p>
+                  <h2 className="font-opens font-semibold text-md   text-2xl">
+                    ${items.price}
+                  </h2>
+                </div>
+              </Link>
             ))}
           </div>
-          <div className=" bg-light-gray">
+          <div className=" bg-light-gray p-2">
             {data.slice(2, 3).map((items) => (
-              <div>
+              <Link to={`/featured/${items.id}`}>
                 <img
                   className="scale-100 hover:scale-110 ease-in duration-500"
                   src={items.img}
                   alt={items.title}
                 />
-              </div>
+                <div className="flex justify-between mt-8">
+                  <h2 className="font-opens font-semibold text-xl">
+                    {items.title}
+                  </h2>
+                  <h2 className="font-opens font-semibold text-md text-gray-600 text-sm md:text-base lg:text-sm xl:text-base 3xl:text-lg"></h2>
+                </div>
+                <div className="flex justify-between  align-middle gap-1">
+                  <p className="font-opens text-gray-600 text-sm mt-2">
+                    {items.description.slice(0, 28)}...
+                  </p>
+                  <h2 className="font-opens font-semibold text-md   text-2xl">
+                    ${items.price}
+                  </h2>
+                </div>
+              </Link>
             ))}
           </div>
-          <div className=" bg-light-gray">
+          <div className=" bg-light-gray p-2">
             {data.slice(3, 4).map((items) => (
-              <div>
+              <Link to={`/featured/${items.id}`}>
                 <img
                   className="scale-100 hover:scale-110 ease-in duration-500"
                   src={items.img}
                   alt={items.title}
                 />
-              </div>
+                <div className="flex justify-between">
+                  <h2 className="font-opens font-semibold text-xl">
+                    {items.title}
+                  </h2>
+                  <h2 className="font-opens font-semibold text-md text-gray-600 text-sm md:text-base lg:text-sm xl:text-base 3xl:text-lg">
+                    $20.00
+                  </h2>
+                </div>
+                <div className="flex justify-between my-2 align-middle gap-1">
+                  <p className="font-opens text-gray-600 text-sm mt-2">
+                    {items.description.slice(0, 30)}...
+                  </p>
+                  <h2 className="font-opens font-semibold text-md   text-2xl">
+                    ${items.price}
+                  </h2>
+                </div>
+              </Link>
             ))}
           </div>
-          <div className=" bg-light-gray">
+          <div className=" bg-light-gray p-2">
             {data.slice(5, 6).map((items) => (
-              <div>
+              <Link to={`/featured/${items.id}`}>
                 <img
                   className="scale-100 hover:scale-110 ease-in duration-500"
                   src={items.img}
                   alt={items.title}
                 />
-              </div>
+                <div className="flex justify-between">
+                  <h2 className="font-opens font-semibold text-xl">
+                    {items.title}
+                  </h2>
+                  <h2 className="font-opens font-semibold text-md text-gray-600 text-sm md:text-base lg:text-sm xl:text-base 3xl:text-lg"></h2>
+                </div>
+                <div className="flex justify-between my-2 align-middle gap-1">
+                  <p className="font-opens text-gray-600 text-sm mt-2">
+                    {items.description.slice(0, 30)}...
+                  </p>
+                  <h2 className="font-opens font-semibold text-md   text-2xl">
+                    ${items.price}
+                  </h2>
+                </div>
+              </Link>
             ))}
           </div>
         </div>
