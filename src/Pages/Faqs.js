92e8d37fff9f4019e69a9908ecc3176d5e32236f
naimur <naimur202163@gmail.com>
2022-11-21 11:42:16 +0600

    working on faw section

---
+++ src/Pages/Faqs.js
@@ -0,0 +1,10 @@
+import React from 'react'
+import Faq from '../components/Faq/Faq'
+
+export default function Faqs() {
+  return (
+    <div>
+      <Faq/>
+    </div>
+  )
+}
--- src/Router.js
+++ src/Router.js
@@ -3,6 +3,8 @@ import { Route, Routes } from "react-router-dom";
 import FeaturedProductsDetails from "./components/Home/FeaturedProductsDetails/FeaturedProductsDetails";
 import FlashSaleDetails from "./components/Home/FlashSaleDetails/FlashSaleDetails";
 import NotFound from "./components/NotFound/NotFound";
+import Faqs from "./Pages/Faqs";
+import Faq from "./Pages/Faqs";
 import Home from "./Pages/Home";
 import ContactUs from "./Shared/ContactUs/ContactUs";
 
@@ -14,6 +16,7 @@ export default function Router() {
         <Route path="/sale/:id" element={<FlashSaleDetails />} />
         <Route path="/featured/:id" element={<FeaturedProductsDetails />} />
         <Route path="/contact" element={<ContactUs />} />
+        <Route path="/faq" element={<Faqs />} />
         <Route path="*" element={<NotFound />} />
       </Routes>
     </>
